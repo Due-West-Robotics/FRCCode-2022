104f71bc0cf94e6b46dc6c632bd72d89780e51e9
DueWesternersProgramming <72952036+DueWesternersProgramming@users.noreply.github.com>
2022-04-07 20:00:33 -0400

    Removed Broken TurnDegrees and cleaned up dependencies

--- src/main/java/frc/robot/commands/Auto/AutoTest.java
+++ src/main/java/frc/robot/commands/Auto/AutoTest.java
@@ -3,21 +3,20 @@ package frc.robot.commands.Auto;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import edu.wpi.first.wpilibj2.command.WaitCommand;
-import frc.robot.Constants.ShooterConstants;
-import frc.robot.commands.Teleop.Intake.StartTransport;
-import frc.robot.commands.Teleop.Intake.StopTransport;
-import frc.robot.commands.Teleop.Shooter.ShooterHoodDown;
-import frc.robot.commands.Teleop.Shooter.StartShooter;
-import frc.robot.commands.Teleop.Shooter.StopShooter;
+import frc.robot.Constants.*;
+import frc.robot.commands.Teleop.Intake.*;
+import frc.robot.commands.Teleop.Shooter.*;
 import frc.robot.subsystems.DriveSubsystem;
 import frc.robot.subsystems.IntakeSubsystem;
 import frc.robot.subsystems.ShooterSubsystem;
 
+/**
+ * A command group used for autonomous testing
+ */
 public class AutoTest extends SequentialCommandGroup{
-    
     public AutoTest(DriveSubsystem m_drive, ShooterSubsystem m_shooter, IntakeSubsystem m_intake) {
         addCommands(
-            new TurnToHeading(m_drive, new Rotation2d(Math.PI / 2), 0.2)
-        );
-      }
+                new TurnToHeading(m_drive, new Rotation2d(Math.PI / 2), 0.2)
+                );
+    }
 }
